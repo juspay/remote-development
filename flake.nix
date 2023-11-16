c6ebf57efa1cc2e6af984386792ed2cb201f1a03
shivaraj-bh <sbh69840@gmail.com>
2023-11-16 19:04:54 +0530

    remove modules not needed for minimal config

--- flake.nix
+++ flake.nix
@@ -12,8 +12,6 @@
           disko.nixosModules.disko
           ({ modulesPath, ... }: {
             imports = [
-              (modulesPath + "/installer/scan/not-detected.nix")
-              (modulesPath + "/profiles/qemu-guest.nix")
               ./disk-config.nix
             ];
             services.openssh.enable = true;
