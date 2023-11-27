ca5d5940b75095dd207f82cbaf1440b79b18c108
faisalsaifii <m.faisal.s@outlook.com>
2023-11-27 11:02:58 +0530

    move configuration to separate file

--- flake.nix
+++ flake.nix
@@ -7,9 +7,6 @@
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, disko, deploy-rs, ... }:
-    let
-      users = import ./nix/users.nix;
-    in
     {
       deploy.nodes = {
         nixos-remote-01 = {
@@ -32,36 +29,7 @@
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
-          ({ modulesPath, ... }: {
-            imports = [
-              ./nix/disk-config.nix
-            ];
-            networking.hostName = "nixos-remote-01";
-            services.openssh.enable = true;
-            services.tailscale.enable = true;
-            users.users = {
-              root = {
-                openssh.authorizedKeys.keys = [
-                  users.shivaraj.pubKey
-                  users.faisal.pubKey
-                ];
-              };
-              faisal = {
-                openssh.authorizedKeys.keys = [
-                  users.faisal.pubKey
-                ];
-                isNormalUser = true;
-              };
-            };
-            boot.loader.grub = {
-              # adding devices is managed by disko
-              # devices = [ ];
-              efiSupport = true;
-              efiInstallAsRemovable = true;
-            };
-            system.stateVersion = "23.11";
-          }
-          )
+          ./nix/configuration.nix
         ];
       };
     };
