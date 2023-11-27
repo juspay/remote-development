33111ac4b9383d9b0d0d7626913c61c9b77445b2
Sridhar Ratnakumar <srid@srid.ca>
2023-11-27 09:24:56 -0500

    Consistent brace indentation

--- flake.nix
+++ flake.nix
@@ -6,31 +6,30 @@
     deploy-rs.url = "github:serokell/deploy-rs";
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, disko, deploy-rs, ... }:
-    {
-      deploy.nodes = {
-        nixos-remote-01 = {
-          hostname = "100.92.110.74"; # Tailscale IP belongs to separate Tailscale account with gmail login shared between Faisal & Shivaraj
-          sshUser = "root";
-          profiles.system = {
-            user = "root";
-            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nixos-remote-01;
-          };
+  outputs = { self, nixpkgs, disko, deploy-rs, ... }: {
+    deploy.nodes = {
+      nixos-remote-01 = {
+        hostname = "100.92.110.74"; # Tailscale IP belongs to separate Tailscale account with gmail login shared between Faisal & Shivaraj
+        sshUser = "root";
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nixos-remote-01;
         };
-      };
-      apps."x86_64-linux" = {
-        # Deploy
-        default = {
-          type = "app";
-          program = nixpkgs.lib.getExe deploy-rs;
-        };
-      };
-      nixosConfigurations.nixos-remote-01 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./nix/configuration.nix
-        ];
       };
     };
+    apps."x86_64-linux" = {
+      # Deploy
+      default = {
+        type = "app";
+        program = nixpkgs.lib.getExe deploy-rs;
+      };
+    };
+    nixosConfigurations.nixos-remote-01 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./nix/configuration.nix
+      ];
+    };
+  };
 }
