1a3319ee534dcd0f4950d6b319f8dddf42ccdcab
Sridhar Ratnakumar <srid@srid.ca>
2024-02-27 10:53:43 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -21,14 +21,16 @@
       # Deploy
       default = {
         type = "app";
-        program = nixpkgs.lib.getExe deploy-rs;
+        program = nixpkgs.lib.getExe deploy-rs.packages.x86_64-linux.deploy-rs;
       };
     };
     nixosConfigurations.nixos-remote-01 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       modules = [
         disko.nixosModules.disko
         ./nix/configuration.nix
+        {
+          nixpkgs.hostPlatform = "x86_64-linux";
+        }
       ];
     };
   };
