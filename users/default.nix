b5c7275d84b663c62b6f24fdafb0f6367112ec51
Sridhar Ratnakumar <srid@srid.ca>
2024-02-28 00:24:30 +0530

    note: Use module system

--- users/default.nix
+++ users/default.nix
@@ -1,7 +1,8 @@
 { lib, ... }:
 
 let
-  # TODO: compute this using builtins.readDir
+  # TODO: compute this using builtins.readDir?
+  # Or just use the module system.
   users = {
     srid = import ./srid.nix;
     shivaraj = import ./shivaraj.nix;
