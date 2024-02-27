65bcab383f43d68fa65fb1ef1e187a15ecd57053
Sridhar Ratnakumar <srid@srid.ca>
2024-02-27 11:32:52 -0500

    disable gui/printing/sound

--- hosts/vanjaram/configuration.nix
+++ hosts/vanjaram/configuration.nix
@@ -31,32 +31,6 @@
     LC_TIME = "en_IN";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
   nix = {
     settings.experimental-features = [ "nix-command" "flakes" ];
   };
@@ -68,13 +42,6 @@
 
   services.tailscale.enable = true;
   services.openssh.enable = true;
-
-  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-  # If no user is logged in, the machine will power down after 20 minutes.
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.remotedev = {
