424fe0d2147d6e7f2aabda6a80d83cb03e34533b
Sridhar Ratnakumar <srid@srid.ca>
2024-11-09 03:21:38 +0530

    workaround nm issue

--- nixos/common.nix
+++ nixos/common.nix
@@ -54,4 +54,6 @@
   # vscode-server workaround:
   # https://github.com/nix-community/nixos-vscode-server/issues/67#issuecomment-1763115054
   programs.nix-ld.enable = true;
+
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
